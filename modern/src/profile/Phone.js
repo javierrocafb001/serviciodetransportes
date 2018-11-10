3aa8bcde2a4ade7a2c2f615666e708628b03169b
Javier <javierroca340@yahoo.com>
2018-11-10 12:43:09 -0600

    commit

--- modern/src/profile/Phone.js
+++ modern/src/profile/Phone.js
@@ -7,7 +7,7 @@
         game: "Admin.view.phone.game.Game",
 	email: "Admin.view.phone.email.Email",
         reportcontrol: "Admin.view.phone.email.ReportControl",
-        reportpercontrol: "Admin.view.phone.email.ReportPerControl",
+	reportpercontrol: "Admin.view.phone.email.ReportPerControl",
         inbox: "Admin.view.phone.email.Inbox",
         compose: "Admin.view.phone.email.Compose",
         searchusers: "Admin.view.phone.search.Users"
